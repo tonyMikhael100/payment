219433daab7c62b220a6601ad5d84d5edc99d168
tonyMikhael100 <tonyMikhael100@gmail.com>
2025-04-01 22:07:57 +0200

    finish stripe 3 basic steps

---
+++ lib/core/utils/api_keys.dart
@@ -0,0 +1,4 @@
+class ApiKeys {
+  static String secretKey =
+      'sk_test_51R96SGQufVJqh3b4KuicbHViq9Qgw7PqnCsnkUtvo1vtIsNULkYoCEgvSv9L9LNyc9zPWW3O8Rbn2e023aVqV3Jt00yd3e6596';
+}
--- lib/core/utils/stripe_service.dart
+++ lib/core/utils/stripe_service.dart
@@ -1,1 +1,43 @@
-class StripeService {}
+import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:payment_app/core/utils/api_keys.dart';
+import 'package:payment_app/core/utils/api_service.dart';
+import 'package:payment_app/features/checkout%20features/data/models/payment_intent_input_model.dart';
+import 'package:payment_app/features/checkout%20features/data/models/payment_intent_model/payment_intent_model.dart';
+
+class StripeService {
+  final ApiService apiService = ApiService();
+  Future<PaymentIntentModel> createPaymentIntent(
+    PaymentIntentInputModel paymentIntentInputModel,
+  ) async {
+    var response = await apiService.post(
+      body: paymentIntentInputModel.toJson(),
+      url: 'https://api.stripe.com/v1/payment_intents',
+      token: ApiKeys.secretKey,
+    );
+    var paymentIntentModel = PaymentIntentModel.fromJson(response.data);
+    return paymentIntentModel;
+  }
+
+  Future initPaymentSheet({required String paymentIntentClientSecret}) async {
+    await Stripe.instance.initPaymentSheet(
+      paymentSheetParameters: SetupPaymentSheetParameters(
+        paymentIntentClientSecret: paymentIntentClientSecret,
+        merchantDisplayName: 'Tony Mikhael',
+      ),
+    );
+  }
+
+  Future presentPaymentSheet() async {
+    await Stripe.instance.presentPaymentSheet();
+  }
+
+  Future makeAllPaymentSteps({
+    required PaymentIntentInputModel paymentIntentInputModel,
+  }) async {
+    var paymentIntentModel = await createPaymentIntent(paymentIntentInputModel);
+    await initPaymentSheet(
+      paymentIntentClientSecret: paymentIntentModel.clientSecret!,
+    );
+    await presentPaymentSheet();
+  }
+}
